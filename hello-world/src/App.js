caa337d6ffe1e98bf2d4c08a9b621ea0fe8e9cd5
arvishcdoshi <doshi.arvish16@gmail.com>
2024-05-06 15:06:45 +0530

    Forwarding Refs.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -24,13 +24,16 @@ import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FocusInput />
+        <FRParentInput />
+        {/* <FocusInput /> */}
         {/* <RefsDemo /> */}
+        
         {/* <Table/> */}
         {/* <FragmentDemo /> */}
         {/* <LifecycleA /> */}
