6e9b185acccf13020bc250d282b6773361e1d9e9
arvishcdoshi <doshi.arvish16@gmail.com>
2024-05-18 17:58:51 +0530

    Error Boundaries

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -25,11 +25,25 @@ import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
 import FRParentInput from './components/FRParentInput';
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ErrorBoundary>
+        <Hero heroName="Batman" />
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+        <Hero heroName="Superman" />
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+        <Hero heroName="Joker" />
+        </ErrorBoundary>
+
         {/* <FRParentInput /> */}
         {/* <FocusInput /> */}
         {/* <RefsDemo /> */}
