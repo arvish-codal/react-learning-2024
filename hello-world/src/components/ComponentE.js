a03d344b73ccce22160932dc493310af7606852d
arvishcdoshi <doshi.arvish16@gmail.com>
2024-05-19 16:04:21 +0530

    Context API

--- hello-world/src/components/ComponentE.js
+++ hello-world/src/components/ComponentE.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './userContext'
+import User from './User'
 
 class ComponentE extends Component {
+
+  static contextType = UserContext
+    
   render() {
-    return <ComponentF />
+    return (
+        <div>
+            Component E context {this.context}
+            <ComponentF />
+        </div>
+    )
   }
 }
 
