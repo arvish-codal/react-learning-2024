0ff86a036a4b001063c95f36eb11f1e96151a7b7
arvishcdoshi <doshi.arvish16@gmail.com>
2024-06-01 22:54:54 +0530

    useMemo Hook

--- callback-hook/src/App.js
+++ callback-hook/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import ParentComponent from './components/ParentComponent';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <ParentComponent />
+      {/* <ParentComponent /> */}
+
+      <Counter />
     </div>
   );
 }
