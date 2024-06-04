49ae1efdd5df0b06934ea614b4309c206ba0efbb
arvishcdoshi <doshi.arvish16@gmail.com>
2024-06-04 08:24:19 +0530

    TypeScript - Passing object props & array prop.

--- react-typescript/react-typescript-demo/src/App.tsx
+++ react-typescript/react-typescript-demo/src/App.tsx
@@ -1,10 +1,35 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  
+  const personName = {
+    first: 'Steve',
+    last: 'Rogers',
+  }
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne'
+    },
+    {
+      first: 'Clark',
+      last: 'Kent'
+    },
+    {
+      first: 'Princess',
+      last: 'Diana'
+    },
+]
+
   return (
     <div className="App">
-      <Greet name='arvish' />
+      <Greet name='arvish' messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
