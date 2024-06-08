7d0bad4b742f2b07f5778279b1b5bf14df860081
arvishcdoshi <doshi.arvish16@gmail.com>
2024-06-08 17:55:12 +0530

    Restricting component props in TypeScript

--- react-typescript/react-typescript-demo/src/App.tsx
+++ react-typescript/react-typescript-demo/src/App.tsx
@@ -16,6 +16,7 @@ import { Counter } from './components/class/Counter';
 import { Private } from './components/auth/Private';
 import { Profile } from './components/auth/Profile';
 import { List } from './components/generics/List';
+import { RandomNumber } from './components/restriction/RandomNumber';
 
 function App() {
   
@@ -79,12 +80,15 @@ function App() {
       {/* <Private isLoggedIn={true} component={Profile} /> */}
 
       {/* Changes for Video - 111 */}
-      <List 
+      {/* <List 
         items={['Batman', 'Superman', 'Wonder Woman']} 
         onClick={(item) => console.log(item)}
       />
 
-      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} />
+      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
+
+     {/* Changes for Video - 112 */}
+      <RandomNumber value={10} isPositive={true}/>
 
     </div>
   );
