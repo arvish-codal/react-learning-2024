cf7d8deb306b1fc177363c19220459ba6e85a83a
arvishcdoshi <doshi.arvish16@gmail.com>
2024-06-11 15:46:47 +0530

    Navigating programatically between routes and some tweaks in useNavigate Hook

--- react-router-demo/src/components/Home.js
+++ react-router-demo/src/components/Home.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Home = () => {
-  return <div><h1>Home Page</h1></div>
+  const navigate = useNavigate()
+
+  return (
+    <>
+    <div><h1>Home Page</h1></div>
+    <button onClick={() => navigate('order-summary')}>Place order</button>
+
+    {/* We might also want to replace the history instead of pushing a route onto the stack. For that, we
+        can pass in an optional second argument which is an object with a property called replace set to True 
+        When we click `Go back`, we'll go back to the browser tab instead of the Home page. This is because we
+        replaced instead of pushing order summary onto the history stack */}
+
+    {/* <button onClick={() => navigate('order-summary', {replace: true}) }>Place order</button> */}
+    </>
+  )
 }
