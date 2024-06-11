3ec5c55e0f4c981c81cb1b12cf95faee52cbd19b
arvishcdoshi <doshi.arvish16@gmail.com>
2024-06-11 17:44:40 +0530

    Dynamic Routes

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -7,6 +7,9 @@ import { NoMatch } from './components/NoMatch';
 import { Products } from './components/Products';
 import { FeaturedProducts } from './components/FeaturedProducts';
 import { NewProducts } from './components/NewProducts';
+import { Users } from './components/Users';
+import { UserDetails } from './components/UserDetails';
+import { Admin } from './components/Admin';
 
 function App() {
   return (
@@ -22,7 +25,12 @@ function App() {
          <Route path='featured' element={<FeaturedProducts />} />
          <Route path='new' element={<NewProducts />} />
        </Route>
-       
+
+       <Route path='users' element={<Users />}>
+          <Route path=':userId' element={<UserDetails />} />
+          <Route path='admin' element={<Admin />} />
+       </Route>
+
        <Route path='*' element={<NoMatch />}></Route>
     </Routes>
     </>
