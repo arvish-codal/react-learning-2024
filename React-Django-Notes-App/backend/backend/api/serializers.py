4e8dcfccb921567a1124e99113f386da4f6a8a5c
arvishcdoshi <doshi.arvish16@gmail.com>
2024-06-16 11:17:49 +0530

    Completed Backend code - Notes CRUD API's

--- React-Django-Notes-App/backend/backend/api/serializers.py
+++ React-Django-Notes-App/backend/backend/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from .models import Note
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,3 +11,10 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
