44140fa7095fc9faf4a80eeaf1d20c3994418a36
arvishcdoshi <doshi.arvish16@gmail.com>
2024-06-27 16:12:43 +0530

    Parallel routes.

---
+++ NextJS/hello-world/src/app/complex-dashboard/layout.tsx
@@ -0,0 +1,29 @@
+import UserAnalytics from "@/components/UserAnalytics"
+import RevenueMetrics from "@/components/RevenueMetrics"
+import Notifications from "@/components/Notifications"
+import React from "react"
+
+export default function DashboardLayout({
+    children,
+    users,
+    revenue,
+    notifications
+}: {
+    children: React.ReactNode,
+    users: React.ReactNode,
+    revenue: React.ReactNode,
+    notifications: React.ReactNode
+}) {
+    return (
+        <div>
+          <div>{children}</div>  {/* Content from `page.tsx` */}
+          <div style={{ display: "flex" }}>
+            <div style={{ display: "flex", flexDirection: "column"}}>
+                <div>{users}</div>
+                <div>{revenue}</div>
+            </div>
+            <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
+          </div>
+        </div>
+    )
+}
