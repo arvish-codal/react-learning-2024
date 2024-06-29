f3828ee6f62b36fa7db2df6516b12bfa08d467d8
arvishcdoshi <doshi.arvish16@gmail.com>
2024-06-29 20:29:01 +0530

    Dynamic Route Handlers & Query Params.

--- NextJS/route-handlers/src/app/comments/route.ts
+++ NextJS/route-handlers/src/app/comments/route.ts
@@ -1,7 +1,13 @@
+import { NextRequest } from "next/server";
 import {comments} from "./data"
 
-export async function GET() {
-    return Response.json(comments);
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams
+    const query = searchParams.get("query")
+    const filteredComments = query 
+    ? comments.filter(comment => comment.text.includes(query))
+    : comments
+    return Response.json(filteredComments);
 }
 
 // Every route handler function receives the standard web request object as a parameter.
